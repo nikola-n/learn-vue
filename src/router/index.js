1dc77edfda232d0254e91e6cd3739bf97fcb0256
Nikola <nikola@thecodeconnectors.nl>
2021-12-19 22:39:42 +0100

    whole app

---
+++ src/router/index.js
@@ -0,0 +1,35 @@
+import {createRouter, createWebHashHistory} from "vue-router";
+import EventList from "../views/EventList.vue";
+import About from "../views/About.vue";
+import EventDetails from "@/views/EventDetails.vue";
+
+const routes = [
+    {
+        path: "/",
+        name: "EventList",
+        component: EventList,
+    },
+    {
+        path: "/event/:id",
+        name: "EventDetails",
+        props: true,
+        component: EventDetails,
+    },
+    {
+        path: "/about",
+        name: "About",
+        component: About
+        // route level code-splitting
+        // this generates a separate chunk (about.[hash].js) for this route
+        // which is lazy-loaded when the route is visited.
+        // component: () =>
+        //   import(/* webpackChunkName: "about" */ "../views/About.vue"),
+    },
+];
+
+const router = createRouter({
+    history: createWebHashHistory(process.env.BASE_URL),
+    routes,
+});
+
+export default router;
---
+++ src/services/EventService.js
@@ -0,0 +1,26 @@
+// its not good to load axios inside all components
+// api code is all over the app
+// it can easily break and hard to debug
+// that's why we create this file
+// to call axios only once
+
+import axios from "axios";
+
+const apiClient = axios.create({
+    baseURL: 'https://my-json-server.typicode.com/Code-Pop/Real-World_Vue-3',
+    withCredentials: false,
+    headers: {
+        Accept: 'application/json',
+        'Content-Type': 'application/json',
+    }
+});
+
+export default {
+    getEvents() {
+        return apiClient.get('/events');
+    },
+
+    getEvent(id) {
+        return apiClient.get('/events/' + id);
+    },
+}
